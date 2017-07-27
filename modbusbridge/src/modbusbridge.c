ad6f36846f797ccb0abe7c51f88f9df12e065bb4
georgidimov <gt_dimov@mail.bg>
2017-07-27 17:39:12 +0300

    Add request handling in modbusbridge.

--- modbusbridge/src/modbusbridge.c
+++ modbusbridge/src/modbusbridge.c
@@ -4,10 +4,14 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <modbus/modbus.h>
 
 #define ARGUMENTS_COUNT 8
+
+#define TCP_MAX_CONNECTIONS 512
 
 
 modbus_t * rtu_context = NULL;
@@ -65,8 +69,6 @@ void termination_handler(int signum) {
     modbus_close(tcp_context);
     modbus_free(tcp_context);
     tcp_context = NULL;
-
-    printf("\nCalled %d\n", signum);
 
     exit(signum);
 }
@@ -165,14 +167,6 @@ int main(int argc, char * argv[]) {
         return 2;
     }
 
-    if (modbus_connect(tcp_context) == -1) {
-           printf("Modbus TCP connection failed: %s\n", modbus_strerror(errno));
-           modbus_free(rtu_context);
-           modbus_free(tcp_context);
-           return 2;
-    }
-
-
     struct sigaction signal_action;
     signal_action.sa_handler = termination_handler;
     sigemptyset (&signal_action.sa_mask);
@@ -183,6 +177,127 @@ int main(int argc, char * argv[]) {
     sigaction(SIGHUP, &signal_action, NULL);
 
 
+    int server_socket;
+    //also clear the fd sets
+    fd_set connections;
+    FD_ZERO(&connections);
 
+    fd_set active_connections;
+    FD_ZERO(&active_connections);
+
+    struct sockaddr_in address;
+    socklen_t address_length = sizeof(address);
+
+    int new_fd;
+
+    uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+    int query_length;
+    int raw_query_length;
+
+
+    uint8_t response[MODBUS_RTU_MAX_ADU_LENGTH];
+    int response_length;
+    int raw_response_length;
+
+    int slave_id;
+    int data_start_index;
+    int exception;
+
+
+    server_socket = modbus_tcp_listen(tcp_context, TCP_MAX_CONNECTIONS);
+
+    if (server_socket == -1) {
+        printf("Modbus TCP listen failed: %s\n", modbus_strerror(errno));
+        termination_handler(3);
+    }
+
+    FD_SET(server_socket, &active_connections);
+
+    int i = 0;
+
+    while (1) {
+        connections = active_connections;
+
+        if (select(FD_SETSIZE, &connections, NULL, NULL, NULL) == -1) {
+            printf("Select error: %s\n", strerror(errno));
+            termination_handler(4);
+        }
+
+        for (i = 0; i < FD_SETSIZE; ++i) {
+            if (FD_ISSET(i, &connections)) {
+
+                //Connection on server socket in other words
+                //a client is asking for a new connection
+                if (i == server_socket) {
+
+                    //clear temporary address variable
+                    memset(&address, 0, address_length);
+                    new_fd = accept(server_socket, (struct sockaddr *)&address, &address_length);
+
+                    if (new_fd == -1) {
+                        printf("Accept error: %s\n", strerror(errno));
+                    } else {
+                        FD_SET(new_fd, &active_connections);
+
+                        printf("New connection from %s:%d on socket %d\n", inet_ntoa(address.sin_addr), address.sin_port, new_fd);
+                    }
+
+                // Data arriving on an already-connected socket.
+                } else {
+                    //reset the exection value
+                    exception = 0;
+
+                    modbus_set_socket(tcp_context, i);
+                    query_length = modbus_receive(tcp_context, query);
+
+                    //Close the connection
+                    if (query_length == -1) {
+                        printf("Connection closed on socker %d\n", i);
+
+                        //close the socket and remove it from fd set
+                        close(i);
+                        FD_CLR(i, &active_connections);
+
+                    //Parse the tcp request and send it over rtu context
+                    } else {
+                        data_start_index = modbus_get_header_length(tcp_context) - 1;
+
+                        slave_id = query[data_start_index];
+                        raw_query_length = query_length - data_start_index - modbus_get_checksum_length(tcp_context);
+
+                        modbus_flush(rtu_context);
+                        modbus_set_slave(rtu_context, slave_id);
+
+                        if (modbus_send_raw_request(rtu_context, query + data_start_index, raw_query_length) == -1) {
+                            printf("Modbus RTU send request error: %s\n", modbus_strerror(errno));
+                            exception = errno;
+                        } else {
+                            response_length = modbus_receive_confirmation(rtu_context, response);
+
+                            if (response_length == -1) {
+                                printf("Modbus RTU receive responsed error: %s\n", modbus_strerror(errno));
+                                exception = errno;
+                            } else {
+                                data_start_index = modbus_get_header_length(rtu_context) - 1;
+                                raw_response_length = response_length - data_start_index - modbus_get_checksum_length(rtu_context);
+                                modbus_reply_raw_response(tcp_context, query, query_length, response + data_start_index, response_length);
+                            }
+                        }
+
+                        //If exists exception send it in modbus
+                        if (exception != 0) {
+                            if (exception > MODBUS_ENOBASE && MODBUS_ENOBASE < (MODBUS_ENOBASE + MODBUS_EXCEPTION_MAX)) {
+                                exception -= MODBUS_ENOBASE;
+                            } else {
+                                exception = EMBXSFAIL;
+                            }
+
+                            modbus_reply_exception(tcp_context, query, exception);
+                        }
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
