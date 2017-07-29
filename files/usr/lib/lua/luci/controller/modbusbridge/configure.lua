1d2781b6d905f4a55930d746d3c6ab2693a1d01d
georgidimov <gt_dimov@mail.bg>
2017-07-29 19:20:42 +0300

    Remove useless lines in modbus controller.

--- files/usr/lib/lua/luci/controller/modbusbridge/configure.lua
+++ files/usr/lib/lua/luci/controller/modbusbridge/configure.lua
@@ -1,20 +1,5 @@
 module("luci.controller.modbusbridge.configure", package.seeall)
 
 function index()
---    entry({"modbusbridge", "configure"}, cbi("modbusbridge-configure/configure"), "Modbus Bridge Configuration", 30).dependent=false
     entry({"admin", "services", "modbusbridge"}, cbi("modbusbridge-configure/configure"), "Modbusbridge", 30).dependent=false
-    entry({"admin", "services", "modbusbridge", "reload"}, call("reload_modbusbridge")).dependent=false
-end
-
-
-function reload_modbusbridge()
-    local conn = ubus.connect()
-
-    if not conn then
-	error("Failed to connect to ubusd")
-    end
-
-    conn:call("modbusbridge", "reload", { })
-
-    conn:close()
 end
