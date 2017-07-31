574b08afcc79a92d50fd3c29fde877d656526782
georgidimov <gt_dimov@mail.bg>
2017-07-31 15:54:32 +0300

    Move modbusbridge package files into subdirectory.

--- files/usr/lib/lua/luci/controller/modbusbridge/configure.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.modbusbridge.configure", package.seeall)
-
-function index()
-    entry({"admin", "services", "modbusbridge"}, cbi("modbusbridge-configure/configure"), "Modbusbridge", 30).dependent=false
-end
